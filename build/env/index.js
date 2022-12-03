f688103ad1061033b1397130030fdbdc532502bc
Vấn Kute <nvvan.0901.developer@gmail.com>
2022-12-04 00:47:38 +0700

    edit build project

--- build/env/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-const NODE_ENV = process.env.NODE_ENV;
-const ConfigEnv = require(`../env/${NODE_ENV}.json`);
-exports.default = ConfigEnv;
