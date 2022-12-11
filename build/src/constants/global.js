2a8e475cd1856f27bc9391b7ae9777db723387bb
Vấn Kute <nvvan.0901.developer@gmail.com>
2022-12-11 15:31:14 +0700

    deploy build

---
+++ build/src/constants/global.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TYPE_EVENT = void 0;
+exports.TYPE_EVENT = {
+    TRANSFER: "Transfer",
+    SALE_KNIGHT: "SaleKnight",
+    DESTROY_SALE_KNIGHT: "DestroySaleKnight",
+    BUY_KNIGHT: "BuyKnight",
+    NEW_KNIGHT: "NewKnight",
+    LEVELUP: "LevelUp",
+    REQUEST_MARRY: "RequestMarry",
+    APPROVAL_MARRY: "ApprovalMarry",
+    BATTLERESULTS: "battleResults",
+    TRIGGER_COOLDOWN: "TriggerCoolDown",
+    TRIGGER_TIRED: "TriggerTired",
+};
